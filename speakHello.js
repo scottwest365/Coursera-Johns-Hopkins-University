7961538704934881a0e438aadbd6239f75664fb1
scottwest365 <davidstone117@protonmail.com>
2025-11-10 11:56:13 -0800

    Update speakHello.js

--- speakHello.js
+++ speakHello.js
@@ -1,9 +1,2 @@
-(function (window) {  
-    var helloSpeaker = {};
-    var speakWord = "Hello";
-    
-    helloSpeaker.speak = function (names) {
-      console.log(speakWord + " " + names);
-    }
-        window.helloSpeaker = helloSpeaker;
-    })(window);
+Delete
+
